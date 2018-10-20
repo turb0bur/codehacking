beee446f43876b56aae5c40cdcce88b0fc8305ed
Volodymyr Butko <turb0bur@ukr.net>
2018-10-20 22:45:04 +0300

    Fixed user.create view due to errors block in accordance to user.update view

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -5,39 +5,42 @@
 @endsection
 
 @section('content')
-    {!! Form::open(['method' =>'post', 'action' => 'AdminUsersController@store', 'files' => 'true']) !!}
+    <div class="row">
+        {!! Form::open(['method' =>'post', 'action' => 'AdminUsersController@store', 'files' => 'true']) !!}
 
-    <div class="form-group">
-        {!! Form::label('name', 'Name:') !!}
-        {!! Form::text('name', null, ['class' =>'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('email', 'Email:') !!}
-        {!! Form::text('email', null, ['class' =>'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('role_id', 'Role:') !!}
-        {!! Form::select('role_id',[''=> 'Choose a role'] + $roles, null, ['class' =>'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('is_active', 'Status:') !!}
-        {!! Form::select('is_active', [0 => 'Not Active', 1 => 'Active'],0, ['class' =>'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('photo_id', 'Upload a Photo', ['class' => 'btn btn-default']) !!}
-        {!! Form::file('photo_id', ['hidden']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('password', 'Password:') !!}
-        {!! Form::password('password', ['class' =>'form-control']) !!}
+        <div class="form-group">
+            {!! Form::label('name', 'Name:') !!}
+            {!! Form::text('name', null, ['class' =>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('email', 'Email:') !!}
+            {!! Form::text('email', null, ['class' =>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('role_id', 'Role:') !!}
+            {!! Form::select('role_id',[''=> 'Choose a role'] + $roles, null, ['class' =>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('is_active', 'Status:') !!}
+            {!! Form::select('is_active', [0 => 'Not Active', 1 => 'Active'],0, ['class' =>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('photo_id', 'Upload a Photo', ['class' => 'btn btn-default']) !!}
+            {!! Form::file('photo_id', ['hidden']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('password', 'Password:') !!}
+            {!! Form::password('password', ['class' =>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::submit('Create User', ['class' => 'btn btn-primary']) !!}
+        </div>
+
+        {!! Form::close() !!}
     </div>
 
-    <div class="form-group">
-        {!! Form::submit('Create User', ['class' => 'btn btn-primary']) !!}
+    <div class="row">
+        @include('partials.form_errors')
     </div>
-
-    {!! Form::close() !!}
-
-    @include('partials.form_errors')
-
 @endsection
