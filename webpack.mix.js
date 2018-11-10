d7573038636def299e48983183742947c3f0c3f4
Volodymyr Butko <turb0bur@ukr.net>
2018-11-10 20:35:07 +0200

    Updated node version and its packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 /**
  * Created by edwin on 3/26/17.
  */
-const { mix } = require('laravel-mix');
+let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css');
